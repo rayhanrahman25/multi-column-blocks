8ccbccee7c78c23f25eb413f09ce2f1cd235c164
rayhanrahman25 <rayhanrahman880@gmail.com>
2024-03-07 19:32:30 +0600

    feat: gutenberg number control created

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '783ca3c5420bfcd04b38');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '06c760a27985838170a1');
